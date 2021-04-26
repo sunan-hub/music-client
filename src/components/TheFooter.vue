b0ec7b7b4e885d82a852186d5b020a6b4f87cf75
sunan <1816546887@qq.com>
2021-04-26 14:49:49 +0800

    feat: 第一次提交，完成登录注册

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="the-footer">
+    <p>放松与激情</p>
+    <p>Ctrl+C ©2021</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'the-footer'
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/css/the-footer.scss';
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,84 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/pages/Home'
+import MyMusic from '@/pages/MyMusic'
+import Singer from '@/pages/Singer'
+import SongList from '@/pages/SongList'
+import Search from '@/pages/Search'
+import Lyric from '@/pages/lyric'
+import SignUp from '@/pages/SignUp'
+import LoginIn from '@/pages/LoginIn'
+import Setting from '@/pages/Setting'
+import SingerAlbum from '@/pages/SingerAlbum'
+import SongListAlbum from '@/pages/SongListAlbum'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/my-music',
+      name: 'my-music',
+      component: MyMusic
+    },
+    {
+      path: '/singer',
+      name: 'singer',
+      component: Singer
+    },
+    {
+      path: '/song-list',
+      name: 'song-list',
+      component: SongList
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
+    },
+    {
+      path: '/lyric',
+      name: 'lyric',
+      component: Lyric
+    },
+    {
+      path: '/sign-up',
+      name: 'sign-up',
+      component: SignUp
+    },
+    {
+      path: '/login-in',
+      name: 'login-in',
+      component: LoginIn
+    },
+    {
+      path: '/setting',
+      name: 'setting',
+      component: Setting
+    },
+    {
+      path: '/singer-album/:id',
+      name: 'singer-album',
+      component: SingerAlbum
+    },
+    {
+      path: '/song-list-album/:id',
+      name: 'song-list-album',
+      component: SongListAlbum
+    }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
+})
+
+// 重复点击导航时，控制台出现报错 ，虽然不影响功能使用，但也不能视而不见
+const VueRouterPush = Router.prototype.push
+Router.prototype.push = function push(to) {
+  return VueRouterPush.call(this, to).catch(err => err)
+}
